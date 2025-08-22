c23dc052077e7dbd7b2ca201b12940a196aa7755
oumy25-rgb <oumemagharsrllaoui@gmail.com>
2025-08-22 09:22:22 +0200

    commenti aggiunti

--- TheKnife/TheKnife/src/resources/GestioneMenu.java
+++ TheKnife/TheKnife/src/resources/GestioneMenu.java
@@ -13,8 +13,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * La classe <strong>GestioneMenu</strong> gestisce la persistenza dei menu
+ * dei ristoranti su file CSV all’interno della cartella <code>src/dati/</code>.
+ * <p>
+ * Le operazioni principali sono:
+ * <ul>
+ *   <li>Scrittura del menu di un ristorante</li>
+ *   <li>Verifica dell’esistenza di un menu</li>
+ *   <li>Lettura del menu da file</li>
+ * </ul>
+ * Ogni riga del file rappresenta un {@link Piatto}.
+ * </p>
+ *
+ * @see resources.Piatto
+ * 
+ * @author omema gharsellaoui
+ * @author Giuseppina Salvati
+ */
+
 public class GestioneMenu {
     // Metodo per scrivere il menu di un ristorante in un file CSV
+
+    /**
+     * Salva il menu di un ristorante in un file CSV.
+     * <p>
+     * Se il file esiste già, viene sovrascritto.
+     * Ogni piatto è salvato nella forma: <br>
+     * <code>nome,descrizione,prezzo</code>
+     * </p>
+     *
+     * @param nomeFile nome del file (senza percorso)
+     * @param piatti lista di piatti da scrivere
+     */
     public void scriviMenu(String nomeFile, ArrayList<Piatto> piatti) {
     try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/dati/" + nomeFile, false))) {
         for (Piatto piatto : piatti) {
@@ -25,7 +56,12 @@ public class GestioneMenu {
         System.err.println("Errore durante la scrittura del menu: " + e.getMessage());
     }
 }
-
+    /**
+     * Controlla se esiste un file menu per un determinato ristorante.
+     *
+     * @param nome nome del file menu
+     * @return true se il file esiste, false altrimenti
+     */
     public static boolean cercaMenu(String nome) {
         
         File file = new File("src/dati", nome);
@@ -36,7 +72,16 @@ public class GestioneMenu {
         }
         
     }
-
+     /**
+     * Legge un menu da un file CSV.
+     * <p>
+     * Ogni riga deve contenere tre campi separati da virgola: <br>
+     * <code>nome,descrizione,prezzo</code>
+     * </p>
+     *
+     * @param nomeFile nome del file CSV del menu
+     * @return lista di {@link Piatto} caricati dal file
+     */
     // Metodo per leggere il menu da un file CSV
     public ArrayList<Piatto> leggiMenu(String nomeFile) {
         ArrayList<Piatto> menu = new ArrayList<>();
@@ -57,4 +102,5 @@ public class GestioneMenu {
         return menu;
     }
 }
+
 
